e35b2e78d83f54c5ad2e42ca42bee42850a9c29b
aziz-shoko <azizbek.shokosimov@gmail.com>
2023-10-31 11:05:29 -0500

    added the 7th assignment, calculator

--- webfiles/javascript/a7.js
+++ webfiles/javascript/a7.js
@@ -14,3 +14,37 @@ function addNumbers() {
     // put a + sign in the sign span
     document.getElementById("sign").innerHTML = "+";
 }
+
+function substract() {
+    var value1 = parseFloat(document.getElementById("box1").value);
+    var value2 = parseFloat(document.getElementById("box2").value);
+    var difference = value1 - value2
+
+    document.getElementById("answer").innerHTML = difference;
+    document.getElementById("sign").innerHTML = "-";
+}
+
+function multiply() {
+    var value1 = parseFloat(document.getElementById("box1").value);
+    var value2 = parseFloat(document.getElementById("box2").value);
+    var product = value1 * value2
+
+    document.getElementById("answer").innerHTML = product;
+    document.getElementById("sign").innerHTML = "*";
+}
+
+function divide() {
+    var value1 = parseFloat(document.getElementById("box1").value);
+    var value2 = parseFloat(document.getElementById("box2").value);
+    var quotient = value1 / value2
+
+    document.getElementById("answer").innerHTML = quotient;
+    document.getElementById("sign").innerHTML = "/";
+}
+
+function clearValues() {
+    document.getElementById("box1").value = "";
+    document.getElementById("box2").value = "";
+    document.getElementById("answer").innerHTML = "";
+    document.getElementById("sign").innerHTML = "&nbsp;";
+}
