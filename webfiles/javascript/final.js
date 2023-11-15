1c846f78be18de0b9becb223042d5c30df444e59
aziz-shoko <azizbek.shokosimov@gmail.com>
2023-11-15 12:08:34 -0600

    added the final project

---
+++ webfiles/javascript/final.js
@@ -0,0 +1,5 @@
+// windows.onload makes sure to wait for the page to laod first before executing the fade features
+window.onload = function() {
+    document.getElementById("logo").style.opacity = 1;
+    document.getElementById("background-circle").style.opacity = 1;
+}
